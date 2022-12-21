118f7e7331a595dd1ac0b965b98b3d77b5995ad3
Aryan Bharti <yoprince.ab@gmail.com>
2022-12-22 00:12:52 +0530

    did a lot of stuff

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-import django_shortcuts
+import os
+from django_shortcuts import render
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session.__init__ import Session
+
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
